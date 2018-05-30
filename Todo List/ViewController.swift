bda60e1c97fe10f9a66b1f4fad0c7b5584a4212f
Sergey Melnik <s.melnik@aimprosoft.com>
2018-05-30 11:10:00 +0300

    Initial project commit.

---
+++ Todo List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todo List
+//
+//  Created by Sergey Melnik on 5/30/18.
+//  Copyright © 2018 Sergey Melnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
